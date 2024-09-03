16739975a9b694729ffb0991a3ad0fe1e57e49cb
jorgeferminrl <jorgeferminrl@gmail.com>
2024-09-03 15:02:36 -0700

    04 - Constants - Like a variable, but its value cannot change

---
+++ js/04-constants.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Constants
+// Like a variable, but its value cannot change
+ var web = "https://wwww.google.com"
+
+ const ip = "192.168.0.12";
+
+ web = "https://javascriptlearningjourney.com"
+
+ console.log(web, ip);
