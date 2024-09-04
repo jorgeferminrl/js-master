35be6a7a86efe5f339e59b7b8b6ab5a5accc49eb
jorgeferminrl <jorgeferminrl@gmail.com>
2024-09-04 14:42:35 -0700

    09 - While

--- js/09-while.js
+++ js/09-while.js
@@ -14,6 +14,20 @@ while (year != 1991) {
 
     console.log("Year is " + year);
 
+    if(year==2000){
+        break;
+    }
+
     year--;
 
 }
+
+// Do while
+
+var years = 30;
+
+do {
+
+    alert("This number is different than 20");
+    years--;
+} while (years > 25)
